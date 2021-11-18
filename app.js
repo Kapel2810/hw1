d44dd20f625eb374d622c7d7b2b65c0d4485fd18
Katrina Apel <kapel2810@gmail.com>
2021-11-18 10:27:30 +0200

    ex2 done

--- app.js
+++ app.js
@@ -1,19 +1,32 @@
 
+// ------ ex 1------
+// let inputElement = document.getElementById('input')
 
+// pBoxElement= document.getElementById("p-box")
+
+// const addCode = () => {
+
+//         let code = inputElement.value
+//     try {
+//        if(isNaN(code))
+//            throw 'רק מספרים מותרים'
+//            code = Number(code)
+//     } catch (error){
+//                 pBoxElement.innerHTML = error
+//                 console.log('error')
+//     }
+// }
+
+// ------ ex 2 ----
+let pBoxElement = document.getElementById('p-box')
 let inputElement = document.getElementById('input')
+let inputName = inputElement.value
 
-pBoxElement= document.getElementById("p-box")
-
-const addCode = () => {
-    
-        let code = inputElement.value
-    try {
-       if(isNaN(code))
-           throw 'רק מספרים מותרים'
-           code = Number(code)
-    } catch (error){
-                pBoxElement.innerHTML = error
-                console.log('error')
-    }
+const addName = () => {
+    console.log('Katrina')
 
 }
+const showName = (callback) => {
+    callback()
+}
+showName(addName)
